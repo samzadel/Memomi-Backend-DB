142ad4edf347bcfcacebb9d48d8c0e9db76d0bf7
samzadel <samzadel@hotmail.fr>
2020-04-07 00:11:42 +0300

    Test api

--- Nodejs_Backend/index.js
+++ Nodejs_Backend/index.js
@@ -1,12 +1,26 @@
-
 const express = require('express')
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
+// const sql = require('mssql')
+const sql = require("msnodesqlv8")
+// const dbConfig = require('./connectToSql')
 const app = express()
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/hava', function (req, res) {
+const connection = "server=.;Database=Memomi;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+
+
+app.post('/hava', function (req, res) {
+  const query = `USE Memomi
+  INSERT INTO dbo.[USER] (EMAIL,PASSWORD,USERNAME,YEAR_OF_BIRTH)
+  VALUES ('${req.body.email}',${req.body.password},'${req.body.username}',${req.body.year_birth})`;
+  console.log(query)
+  sql.query(connection, query, (err, rows) => {
+    if (err) console.log(err)
+    console.log(rows);
+  });
   res.json('hello world')
 })
 
