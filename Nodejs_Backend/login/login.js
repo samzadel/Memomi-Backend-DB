3b49977ce9d5435b8b0b607f986082fc63341248
samzadel <samzadel@hotmail.fr>
2020-04-16 17:27:08 +0300

    login logic

--- Nodejs_Backend/login/login.js
+++ Nodejs_Backend/login/login.js
@@ -5,6 +5,25 @@ const dbConfigConnection = require('../connectToSql')
 const router = express.Router()
 
 
-router.get('/login',(req,res)=>{
+router.post('/login',(req,res)=>{
+    const main = async () => {
+  
+        const pool = await dbConfigConnection.poolConnect;
     
+        const request = new sql.Request(pool);
+
+        const queryCheckExistUser = `USE Memomi 
+        set nocount on;
+        SELECT * FROM [USER] WHERE EMAIL = '${req.body.email}' AND PASSWORD='${req.body.password}'`
+    
+        const result = await request.query(queryCheckExistUser);
+
+        if (result['recordset'].length !== 0){
+          jwt.sign({ user: req.body.email }, 'secretKey', { expiresIn: '60s' }, function (err, token){res.json({ token, message: 'succeed' }) })
+        }
+        else {res.json("email or password is invalid")};      
+
+      };
+      main()
 })
+module.exports = router;
--- Nodejs_Backend/server.js
+++ Nodejs_Backend/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var signUp = require('./signUp/signUp')
+var login = require('./login/login')
 const bodyParser = require("body-parser")
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -8,6 +9,7 @@ app.use(bodyParser.json());
 
 
 app.use('/', signUp);
+app.use('/', login)
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
