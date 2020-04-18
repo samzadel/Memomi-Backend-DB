97e0bdb065c2164640f77ba5b4c2eec6d407363a
samzadel <samzadel@hotmail.fr>
2020-04-18 22:32:09 +0300

    new decryption login

--- Nodejs_Backend/login/login.js
+++ Nodejs_Backend/login/login.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const sql = require('mssql/msnodesqlv8')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const dbConfigConnection = require('../connectToSql')
 const router = express.Router()
 
@@ -12,17 +13,33 @@ router.post('/login',(req,res)=>{
     
         const request = new sql.Request(pool);
 
-        const queryCheckExistUser = `USE Memomi 
+        const queryCheckExistEmail = `USE Memomi 
         set nocount on;
-        SELECT * FROM [USER] WHERE EMAIL = '${req.body.email}' AND PASSWORD='${req.body.password}'`
+        SELECT * FROM [TBL_USER] WHERE EMAIL = '${req.body.email}'`
     
-        const result = await request.query(queryCheckExistUser);
+        const result = await request.query(queryCheckExistEmail);
 
         if (result['recordset'].length !== 0){
-          jwt.sign({ user: req.body.email }, 'secretKey', { expiresIn: '60s' }, function (err, token){res.json({ token, message: 'succeed' }) })
-        }
-        else {res.json("email or password is invalid")};      
+          const queryCheckPwd = `USE Memomi 
+          set nocount on;
+          SELECT PASSWORD FROM [TBL_USER] WHERE EMAIL = '${req.body.email}'`
 
+          const pwd = await request.query(queryCheckPwd)
+
+          const hashPwd = pwd['recordset'][0]['PASSWORD'] 
+
+          await bcrypt.compare(req.body.password,hashPwd).then((resultComparison)=>{
+            if(resultComparison){
+              jwt.sign({ user: req.body.email }, 'secretKey', { expiresIn: '60s' }, function (err, token){res.json({ token, message: 'succeed' })})
+            }else{
+              res.send('Bad password')
+            }
+          })
+
+        }
+        else{
+          res.send('Email doesn\'t found')
+        }     
       };
       main()
 })
